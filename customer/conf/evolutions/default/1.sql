845db465ed6d7f84ebe9eb71f95b49c03e9815d4
Alexandre Santos <alexandre.santos@africainternetgroup.com>
2015-06-12 14:45:18 +0100

    Added migrations to the OmsOrder table

---
+++ customer/conf/evolutions/default/1.sql
@@ -0,0 +1,24 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table oms_order (
+  order_id                  bigint auto_increment not null,
+  order_nr                  varchar(255),
+  product_name              varchar(255),
+  created_at                varchar(255),
+  constraint pk_oms_order primary key (order_id))
+;
+
+
+
+
+# --- !Downs
+
+SET FOREIGN_KEY_CHECKS=0;
+
+drop table oms_order;
+
+SET FOREIGN_KEY_CHECKS=1;
+
