0d8340c8ee6278211194ebdd7f8b14b23021af68
André Marques <amarques@rocket-internet.pt>
2015-06-12 15:13:33 +0100

    List data from db.

--- customer/app/controllers/Application.java
+++ customer/app/controllers/Application.java
@@ -6,22 +6,27 @@ import models.OmsOrder;
 
 import play.*;
 
+import play.db.ebean.Model;
+
 import play.mvc.*;
 
 import views.html.*;
+
+import java.util.List;
+
+import static play.libs.Json.toJson;
 
 
 
 public class Application extends Controller {
 
     public Result index() {
-        
-    	List<OmsOrder> orders = OmsOrder.find.where()
-    	    //.ilike("name", "%sample%")
-    	    //.orderBy("createdAt asc")
-    	    .findList();	
-    	
-    	return ok(index.render("Your new application is ready."));
+        return ok(index.render("Your new application is ready."));
+    }
+    
+    public Result getOrders() {
+    	List<OmsOrder> orders = new Model.Finder(String.class, OmsOrder.class).all();
+    	return ok(toJson(orders));
     }
 
 }
