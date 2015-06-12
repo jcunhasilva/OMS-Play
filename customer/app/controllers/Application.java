715bbe513657d1e7ca1c07d18f0648250a83868f
Jose Silva <jcunhasilva@gmail.com>
2015-06-12 13:40:33 +0100

    Initial version of the order model

--- customer/app/controllers/Application.java
+++ customer/app/controllers/Application.java
@@ -1,14 +1,27 @@
 package controllers;
 
+import java.util.List;
+
+import models.Order;
+
 import play.*;
+
 import play.mvc.*;
 
 import views.html.*;
 
+
+
 public class Application extends Controller {
 
     public Result index() {
-        return ok(index.render("Your new application is ready."));
+        
+    	List<Order> orders = Order.find.where()
+    	    //.ilike("name", "%sample%")
+    	    .orderBy("createdAt asc")
+    	    .findList();	
+    	
+    	return ok(index.render("Your new application is ready."));
     }
 
 }
--- customer/app/models/Order.java
+++ customer/app/models/Order.java
@@ -1,7 +1,32 @@
 package models;
 
-import play.db.ebean.*;
+import java.util.Date;
 
+import javax.persistence.*;
+
+import play.db.ebean.*;
+import play.data.*;
+import play.data.format.*;
+import play.data.validation.*;
+
+
+@Entity
 public class Order extends Model {
+	
+	@Id
+	public Long orderId;
+	  
+	@Constraints.Required
+	public String orderNr;
+	
+	@Constraints.Required
+	public String productName;
+	
+	@Formats.DateTime(pattern="dd/MM/yyyy")
+	public Date createdAt = new Date();
+	
+	public static Finder<Long,Order> find = new Finder<Long, Order>(
+      Long.class, Order.class
+	);
 
 }
